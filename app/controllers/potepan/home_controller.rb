65b831d714e6f026600ce95a72570d09fd10efd0
taka2477 <taka2477@gmail.com>
2018-10-31 11:38:11 +0900

    Added home controller and home/index in view, Updated _header.html, routes
    Homeコントローラ、アクション、ビュー追加。headerロゴからrootへのリンクを実装

---
+++ app/controllers/potepan/home_controller.rb
@@ -0,0 +1,4 @@
+class Potepan::HomeController < ApplicationController
+  def index
+  end
+end
---
+++ spec/controllers/potepan/home_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Potepan::HomeController, type: :controller do
+
+end
