4521ff57fdfb2ea4b33b580e26c67ebbb10af7d0
taka2477 <taka2477@gmail.com>
2018-11-07 16:35:45 +0900

    Initialize branch task3, Add categories_controller, categories/show.html.erb, categories_controller_spec, Update routes(resources :categories, only: :show)
    ブランチtask3作成後、categoriesのコントローラー、show、routesを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # This line mounts Solidus's routes at the root of your application.
+    # This line mounts Solidus's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
@@ -25,5 +25,6 @@ Rails.application.routes.draw do
     get :privacy_policy,            to: 'sample#privacy_policy'
 
     resources :products,            only: [:show]
+    resources :categories,          only: [:show]
   end
 end
