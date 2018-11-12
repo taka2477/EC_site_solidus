e20d97f6d9ab5c1c1ca9d27d621344bb8e01b7fe
taka2477 <taka2477@gmail.com>
2018-11-12 14:12:54 +0900

    Update categorie_controller and categories/show.html
    画像表示実装（未完成）

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,5 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.all
+    @products = Spree::Product.joins(:taxons).where(Spree::Taxon.table_name => { id: @taxon.id })
   end
 end
