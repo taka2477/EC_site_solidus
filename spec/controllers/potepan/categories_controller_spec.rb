7c51855437e85ce5186d5a69d4e9a1c3914b5993
taka2477 <taka2477@gmail.com>
2018-11-14 14:56:57 +0900

    Update categories/_light_section to categories/_partners_logo, categories/show.html, categories_controller_spec, categories_controller
    categoriesのパーシャル修正、categoriesのRspec実装（未完成）、categories_controllerのshowアクション見直し

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::CategoriesController, type: :controller do
+  describe "GET #show" do
+    subject { get :show, { params: params } }
+    let(:params) { { id: categories.id } }
+    let(:categories) { create(:categories) }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    it "response successfully" do
+      expect(response).to be_successful
     end
-  end
 
+    it "returns a 200 response" do
+      expect(response.status). to eq(200)
+    end
+
+    #it "responds correct template" do
+    #  expect(response).to render_template :show
+    #end
+  end
 end
