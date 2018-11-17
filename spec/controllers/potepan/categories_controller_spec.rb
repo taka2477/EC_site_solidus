3f23b93c88859d6465b113d7358f20b5b26ffc8a
taka2477 <taka2477@gmail.com>
2018-11-17 14:21:39 +0900

    Update products/_body.html, categories_controller_spec.rb
    一覧ページへのリンク変更、categoriesコントローラーテスト追加

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Potepan::CategoriesController, type: :controller do
   describe "GET #show" do
-    subject { get :show, { params: params } }
-    let(:params) { { id: categories.id } }
-    let(:categories) { create(:categories) }
+    before { get :show, { params: { id: category.id } } }
+    let(:category) { create(:taxonomy) }
 
     it "response successfully" do
       expect(response).to be_successful
@@ -14,8 +13,8 @@ RSpec.describe Potepan::CategoriesController, type: :controller do
       expect(response.status). to eq(200)
     end
 
-    #it "responds correct template" do
-    #  expect(response).to render_template :show
-    #end
+    it "responds correct template" do
+      expect(response).to render_template :show
+    end
   end
 end
