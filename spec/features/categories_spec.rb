a57aa59bc9c21dda73127fc2ff442b51a355139e
taka2477 <taka2477@gmail.com>
2018-11-19 15:53:34 +0900

    Update features/categories_spec
    テスト追加

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Categories", type: :feature do
   given(:category) { create(:taxonomy, name: "Brand") }
-  given(:brand_kid) { category.root.children.create(name: "Ruby")  }
+  given(:brand_kid) { category.taxons.create(name: "Ruby") }
   given(:ruby_shirt) { brand_kid.leaves.create(name: "Ruby_shirt") }
 
   background do
@@ -13,10 +13,19 @@ RSpec.feature "Categories", type: :feature do
     html = page.html
   end
 
-  # scenario 'shows correct links', js: true do
-    # click_on 'Brand'
-    # expect(page).to have_content 'Ruby'
-    # click_link 'Ruby'
-  # end
+  scenario 'has correct title' do
+    expect(page).to have_title brand_kid.name
+    visit potepan_category_path(ruby_shirt.id)
+    expect(page).to have_title ruby_shirt.name
+  end
 
+  scenario 'shows contents properly' do
+    click_on brand_kid.name
+    expect(current_path).to match potepan_category_path(brand_kid.id)
+    expect(page).to have_content brand_kid.name
+    visit potepan_category_path(ruby_shirt.id)
+    expect(page).to have_content ruby_shirt.name
+    click_on 'BIGBAG Store logo'
+    expect(current_path).to match potepan_path
+  end
 end
