e07d8cd09fea1f6fea1a6fbaccc4ac8c9fcf7382
taka2477 <taka2477@gmail.com>
2018-11-20 15:04:38 +0900

    Add branch task4, Update potepan/products_controller, potepan/products/_related_products.html
    branch４追加, 関連商品mock実装（未完成）

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,7 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
+    @taxon = Spree::Taxon.find(params[:id])
+    @images = Spree::Product.in_taxon(@taxon)
   end
 end
