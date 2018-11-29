032d2e5d2f7b254baa6ba49b570528f7d8ca274a
taka2477 <taka2477@gmail.com>
2018-11-29 22:03:46 +0900

    Update potepan/products_controller, categories/_light_section.html, categories/show.html, products/_product_body.html, products/_related_products.html, products/_thumbnail.html, products/show.html, products_spec
    部分テンプレートのインスタンス変数削除、features/products_spec, products_controller 実装し直し

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,7 +1,8 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
-    @taxons = @product.taxons
-    @related_products = @taxons.flat_map { |taxon| Spree::Product.includes(master: [:images, :default_price]).in_taxon(taxon) }.uniq.reject { |product| product == @product }
+    taxons = @product.taxons
+    @related_products = Spree::Product.in_taxons(taxons).
+      includes(master: [:images, :default_price]).uniq.reject { |product| product == @product }
   end
 end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,18 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
-  given(:brand)    { create(:taxonomy, name: "brands") }
-  given(:category) { create(:taxonomy, name: "categories") }
+  given(:brand)       { create(:taxonomy, name: "brand") }
+  given(:ruby)        { create(:taxon, name: "ruby", parent: brand.root, taxonomy: brand) }
+  given!(:ruby_bag)   { create(:product, name: "ruby_bag", price: '16.66', taxons: [ruby]) }
+  given!(:ruby_shirt) { create(:product, name: "ruby_shirt", price: '17.77', taxons: [ruby]) }
 
-  given(:ruby)  { create(:taxon, name: "ruby", parent: brand.root, taxonomy: brand) }
-  given(:rails) { create(:taxon, name: "rails", parent: brand.root, taxonomy: brand) }
-  given(:bag)   { create(:taxon, name: "bags", parent: category.root, taxonomy: category) }
-  given(:shirt) { create(:taxon, name: "shirt", parent: category.root, taxonomy: category) }
-
-  given!(:rails_tote)  { create(:product, name: "rails_tote", price: '18.88', taxons: [rails, bag]) }
-  given!(:rails_shirt) { create(:product, name: "rails_shirt", price: '15.55', taxons: [rails, shirt]) }
-  given!(:ruby_shirt)  { create(:product, name: "ruby_shirt", price: '17.77', taxons: [ruby, shirt]) }
-  given!(:ruby_bag)    { create(:product, name: "ruby_bag", price: '16.66', taxons: [bag, ruby]) }
+  given(:category)         { create(:taxonomy, name: "category") }
+  given(:shoes)            { create(:taxon, name: "shoes", parent: category.root, taxonomy: category) }
+  given!(:unrelated_shoes) { create(:product, name: "unrelated_shoes", price: '99.99', taxons: [shoes]) }
+  given!(:ruby_shoes)      { create(:product, name: "ruby_shoes", price: '88.88', taxons: [shoes, ruby]) }
 
   background do
     visit potepan_product_path(ruby_shirt.id)
@@ -21,57 +18,44 @@ RSpec.feature "Products", type: :feature do
   scenario 'has correct page title and path' do
     expect(page).to have_current_path(potepan_product_path(ruby_shirt.id))
     expect(page).to have_title ruby_shirt.name
-    click_on 'BIGBAG Store logo'
-    expect(page).to have_current_path(potepan_path)
-    expect(page).to have_title 'BIGBAG Store'
-    click_on 'Home'
-    expect(page).to have_current_path(potepan_path)
-    expect(page).to have_title 'BIGBAG Store'
   end
 
-  scenario 'shows correct product name and link' do
+  scenario 'shows correct main product name and link to Home' do
     within('.lightSection') do
       expect(page).to have_selector 'h2', text: ruby_shirt.name
-      expect(page).not_to have_selector 'h2', text: ruby_bag.name
-      expect(page).to have_selector '.breadcrumb', text: ruby_shirt.name
-      expect(page).not_to have_selector '.breadcrumb', text: ruby_bag.name
-      expect(page).to have_link 'Home'
+      within('.breadcrumb') do
+        expect(page).to have_link 'Home'
+        expect(page).to have_content ruby_shirt.name
+      end
     end
   end
 
-  scenario 'displays the product information properly' do
+  scenario 'displays the contents of main product properly' do
     within('.singleProduct') do
-      # リンク
       expect(page).to have_current_path(potepan_product_path(ruby_shirt.id))
       expect(page).to have_link '一覧ページへ戻る'
-      # product画像
-      expect(page).to have_css '#carousel'
-      expect(page).to have_css '.glyphicon-chevron-left'
-      expect(page).to have_css '.glyphicon-chevron-right'
-      # product概要
       expect(page).to have_selector 'h2', text: ruby_shirt.name
-      expect(page).not_to have_selector 'h2', text: ruby_bag.name
       expect(page).to have_selector 'h3', text: ruby_shirt.price
-      expect(page).not_to have_selector 'h3', text: ruby_bag.price
       expect(page).to have_content ruby_shirt.description
     end
+  end
+
+  scenario 'shows only related_products below the main product' do
     within('.productsContent') do
       # 不適切な商品が表示されていないこと
-      expect(page).not_to have_content ruby_shirt.name
-      expect(page).not_to have_content ruby_shirt.price
-      expect(page).not_to have_content rails_tote.name
-      expect(page).not_to have_content rails_tote.price
-      # 同一ブランド
+      expect(page).not_to have_content unrelated_shoes.name
+      expect(page).not_to have_content unrelated_shoes.price
+      # 関連商品が表示されていること
       expect(page).to have_content ruby_bag.name
       expect(page).to have_content ruby_bag.price
+      expect(page).to have_content ruby_shoes.name
+      expect(page).to have_content ruby_shoes.price
+      # 関連商品のリンクが正しいこと
       click_on ruby_bag.name
       expect(page).to have_current_path(potepan_product_path(ruby_bag.id))
-      # 同一カテゴリ
       visit potepan_product_path(ruby_shirt.id)
-      expect(page).to have_content rails_shirt.name
-      expect(page).to have_content rails_shirt.price
-      click_on rails_shirt.name
-      expect(page).to have_current_path(potepan_product_path(rails_shirt.id))
+      click_on ruby_shoes.name
+      expect(page).to have_current_path(potepan_product_path(ruby_shoes.id))
     end
   end
 end
