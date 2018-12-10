0d2842c153d4f6de22d48aa0d80ad9befdd7f98f
taka2477 <taka2477@gmail.com>
2018-12-10 10:25:05 +0900

    Update app/models/spree/product_decorator.rb
    scope: neat_displayにArel.sqlを追加

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,4 +1,5 @@
 Spree::Product.class_eval do
   scope :includes_img_price, -> { includes(master: [:images, :default_price]) }
-  scope :neat_display, ->(product, number) { where.not(id: product).distinct.order('rand()').limit(number) }
+  scope :neat_display, ->(product, number) { where.not(id: product).
+    distinct.order(Arel.sql('rand()')).limit(number) }
 end
