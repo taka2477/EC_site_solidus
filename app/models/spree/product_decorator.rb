238ff8597d5c372be6dddbd9d2920f9ca555bf98
taka2477 <taka2477@gmail.com>
2018-12-23 13:58:00 +0900

    Update .gitignore for deploy

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,6 +2,6 @@ Spree::Product.class_eval do
   scope :includes_img_price, -> { includes(master: [:images, :default_price]) }
   scope :other_products, ->(product) {
     in_taxons(product.taxons).includes_img_price.
-      where.not(id: product).distinct.order(Arel.sql('rand()'))
+      where.not(id: product).distinct.order(("RANDOM()"))
   }
 end
